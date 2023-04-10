115aa6e2a1c7e87a841a5611c622ac9483a594fc
leyi <leyi@myun.info>
2023-04-10 19:27:11 +0800

    fix bug

--- db-generator/db_schema.sql
+++ db-generator/db_schema.sql
@@ -1,8 +1,5 @@
--- 数据库创建脚本
-drop database if exists easy_front_test_db;
-drop user if exists 'Myun'@'%';
 -- 支持emoji：需要mysql数据库参数： character_set_server=utf8mb4
-create database easy_front_test_db default character set utf8mb4 collate utf8mb4_unicode_ci;
+create database if not exists t_test_db default character set utf8mb4 collate utf8mb4_unicode_ci;
 use easy_front_test_db;
 create user 'Myun'@'%' identified by 'Myun@123jx';
 grant all privileges on easy_front_test_db.* to 'Myun'@'%';
