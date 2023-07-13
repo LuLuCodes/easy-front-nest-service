c1b42fe5507719d06da722a2f75db9b706210703
mading <mading@myun.info>
2023-07-13 16:21:23 +0800

    优化小程序返回结果

--- src/modules/mp/basic/mp.basic.service.ts
+++ src/modules/mp/basic/mp.basic.service.ts
@@ -16,7 +16,6 @@ import {
 } from './mp.basic.dto';
 import { ResponseCode } from '@config/global';
 import { sha1, aes128cbcDecrypt } from '@libs/cryptogram';
-import * as path from 'path';
 
 @Injectable()
 export class MpBasicService {
@@ -37,114 +36,69 @@ export class MpBasicService {
   }
 
   async code2Session(requestBody: Code2SessionDTO): Promise<any> {
-    try {
-      const { appId, code } = requestBody;
-      const core: MPCore = MPCoreFactory.getCore(appId);
-      const data = await BasicAPI.code2Session(core, code);
-      const { openid, session_key } = data;
-      await this.cacheService.set(`session_key#${openid}`, session_key);
-      return { code: ResponseCode.OK, data: { openid }, msg: '' };
-    } catch (error) {
-      return {
-        code: ResponseCode.UNKOWN_ERROR,
-        data: null,
-        msg: error.message,
-      };
-    }
+    const { appId, code } = requestBody;
+    const core: MPCore = MPCoreFactory.getCore(appId);
+    const data = await BasicAPI.code2Session(core, code);
+    const { openid, session_key } = data;
+    await this.cacheService.set(`session_key#${openid}`, session_key);
+    return { openid };
   }
 
   async userDataDecrypt(requestBody: UserDataDecryptDTO): Promise<any> {
-    try {
-      const { openId, rawData, signature, encryptedData, iv } = requestBody;
-      const session_key = await this.cacheService.get(`session_key#${openId}`);
+    const { openId, rawData, signature, encryptedData, iv } = requestBody;
+    const session_key = await this.cacheService.get(`session_key#${openId}`);
 
-      const key = Buffer.from(session_key, 'base64');
-      const baseIv = Buffer.from(iv, 'base64');
+    const key = Buffer.from(session_key, 'base64');
+    const baseIv = Buffer.from(iv, 'base64');
 
-      if (rawData && signature) {
-        const signature2 = sha1(rawData + session_key);
-        if (signature2 !== signature) {
-          return { code: ResponseCode.SIGN_ERROR, msg: '签名错误' };
-        }
+    if (rawData && signature) {
+      const signature2 = sha1(rawData + session_key);
+      if (signature2 !== signature) {
+        throw new Error(`签名错误`);
       }
-
-      const ecrypt = aes128cbcDecrypt(key, baseIv, encryptedData);
-
-      return {
-        code: ResponseCode.OK,
-        data: ecrypt ? JSON.parse(ecrypt) : ecrypt,
-        msg: '',
-      };
-    } catch (error) {
-      return {
-        code: ResponseCode.UNKOWN_ERROR,
-        data: null,
-        msg: error.message,
-      };
     }
+
+    const ecrypt = aes128cbcDecrypt(key, baseIv, encryptedData);
+
+    return ecrypt ? JSON.parse(ecrypt) : ecrypt;
   }
 
   async createQrCode(requestBody: QrCodeDTO): Promise<any> {
-    try {
-      const { appId, path, width } = requestBody;
-      const core: MPCore = MPCoreFactory.getCore(appId);
-      const data = await QrCodeAPI.createQRCode(core, path, width);
-      return { code: ResponseCode.OK, data, msg: '' };
-    } catch (error) {
-      return {
-        code: ResponseCode.UNKOWN_ERROR,
-        data: null,
-        msg: error.message,
-      };
-    }
+    const { appId, path, width } = requestBody;
+    const core: MPCore = MPCoreFactory.getCore(appId);
+    const data = await QrCodeAPI.createQRCode(core, path, width);
+    return data;
   }
 
   async createLimitQrCode(requestBody: LimitQrCodeDTO): Promise<any> {
-    try {
-      const { appId, path, width, autoColor, lineColor, isHyaline } =
-        requestBody;
-      const core: MPCore = MPCoreFactory.getCore(appId);
-      const data = await QrCodeAPI.getWxAcode(
-        core,
-        path,
-        width,
-        autoColor,
-        lineColor,
-        isHyaline,
-      );
-      return { code: ResponseCode.OK, data, msg: '' };
-    } catch (error) {
-      return {
-        code: ResponseCode.UNKOWN_ERROR,
-        data: null,
-        msg: error.message,
-      };
-    }
+    const { appId, path, width, autoColor, lineColor, isHyaline } = requestBody;
+    const core: MPCore = MPCoreFactory.getCore(appId);
+    const data = await QrCodeAPI.getWxAcode(
+      core,
+      path,
+      width,
+      autoColor,
+      lineColor,
+      isHyaline,
+    );
+    return data;
   }
 
   async createUnLimitQrCode(requestBody: UnQrlimitCodeDTO): Promise<any> {
-    try {
-      const { appId, scene, path, width, autoColor, lineColor, isHyaline } =
-        requestBody;
-      const core: MPCore = MPCoreFactory.getCore(appId);
-      const data = await QrCodeAPI.getUnlimited(
-        core,
-        scene,
-        path,
-        false,
-        'develop',
-        width,
-        autoColor,
-        lineColor,
-        isHyaline,
-      );
-      return { code: ResponseCode.OK, data, msg: '' };
-    } catch (error) {
-      return {
-        code: ResponseCode.UNKOWN_ERROR,
-        data: null,
-        msg: error.message,
-      };
-    }
+    const { appId, scene, path, width, autoColor, lineColor, isHyaline } =
+      requestBody;
+    const core: MPCore = MPCoreFactory.getCore(appId);
+    const data = await QrCodeAPI.getUnlimited(
+      core,
+      scene,
+      path,
+      false,
+      'develop',
+      width,
+      autoColor,
+      lineColor,
+      isHyaline,
+    );
+    return data;
   }
 }
